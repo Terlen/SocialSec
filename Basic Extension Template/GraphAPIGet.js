ec4e3cb34ba4199332eb439551a4490a2ba4ccce
acpayne1 <acpayne1@asu.edu>
2016-11-02 12:33:13 -0700

    Added Comments
    Added comments explaining functionality of defined functions.

--- Basic Extension Template/GraphAPIGet.js
+++ Basic Extension Template/GraphAPIGet.js
@@ -1,13 +1,16 @@
+// HTTP GET request function. 'theUrl' is passed from FacebookLogin.js and consists of the desired Graph API endpoint with the user's access token appended.
 function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        // XMLHttpRequests are Asynchronous, so this code detects when the request has successfully completed.
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             callback(xmlHttp.responseText);
     }
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
 }
+// Debugging function that serves as the callback function for httpGetAsync. Generates a popup dialog containing the returned JSON from the API call.
 function GetAlert(response) {
 	alert(response);
 }
