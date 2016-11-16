b4f548b09e58fce12b33be540d14a381ad07440f
Aidan Payne <Terlen@users.noreply.github.com>
2016-11-15 19:44:06 -0700

    Improved functionality across multiple sites
    Removed window.onload dependency to enhance multi site compatibility.
    
    Added a textfile of site logins tested to track compatibility

--- Basic Extension Template/passwordDetect.js
+++ Basic Extension Template/passwordDetect.js
@@ -1,8 +1,13 @@
-window.onload = function(){
+
+function passwordGrab(){
+	//alert(window.onload);
 	// Locate input fields with type "password". Stores fields as a static NodeList object
 	var password = document.querySelectorAll('input[type=password]');
-	// Adds a listener on the first occurance of a password field that triggers when field focus is lost. Calls alertContent on trigger
+	//alert(password);
+	//password[0].addEventListener("focus",function(){});
+	// Adds a listener on the first occurrence of a password field that triggers when field loses focus.
 	password[0].addEventListener("focusout",alertContent);
+		
 	
 	// Debugging function to create alert box with content of password field. 
 	function alertContent(){
@@ -10,3 +15,4 @@ window.onload = function(){
 	}
 	
 }
+passwordGrab();
