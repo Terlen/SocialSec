6d8f359128e183d2db007e68ea87c9a3ecfd92e9
Terlen <paidan@ymail.com>
2016-11-27 09:18:36 -0800

    Comment Cleanup and Additions
    Removed commented debug code from earlier testing.
    
    Added some additional explanation to existing comments

--- Basic Extension Template/passwordDetect.js
+++ Basic Extension Template/passwordDetect.js
@@ -1,19 +1,15 @@
-
+// This is a content script that runs on all webpages visited by the user.
 function passwordGrab(){
-	//alert(window.onload);
-	// Locate input fields with type "password". Stores fields as a static NodeList object
+	// Locate input fields with type "password". Stores list of fields as a static NodeList object
 	var password = document.querySelectorAll('input[type=password]');
-	//alert(password);
-	//password[0].addEventListener("focus",function(){});
-	// Adds a listener on the first occurrence of a password field that triggers when field loses focus.
+	// Adds a listener on the first occurrence of a password field that triggers when field loses focus. This is intended to trigger on user clicking on submit button for login form.
 	password[0].addEventListener("focusout",alertContent);
 		
 	
-	// Debugging function to create alert box with content of password field. 
+	// Debugging function to create alert box with content of password field. Used to verify code is working as intended. Field content will later be compared against wordlist.
 	function alertContent(){
 		alert(password[0].value);
 	}
 	
 }
 passwordGrab();
-// This is a comment
