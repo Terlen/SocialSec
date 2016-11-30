fdd9a4f1a7bef11e9f7a0ead728d89801d108b44
Aidan Payne <Terlen@users.noreply.github.com>
2016-11-30 12:30:03 -0700

    Added request for work and education
    Added requests for work history and education from the GraphAPI.
    
    These requests will not work for any users outside the development team.

--- Basic Extension Template/facebookLogin.js
+++ Basic Extension Template/facebookLogin.js
@@ -5,14 +5,14 @@ window.onload = function() {
 		/* This url is a Facebook OAuth endpoint that generates an access token for the user with the desired permissions. In this request, scope defines the rights requested by the application.
 		   We currently generate a token that allows access to email, public_profile information, and user_friends. The redirect_uri is the page that the user is redirected to after authentication.
 		   In our case, we are using a page designed by Facebook for desktop applications which don't have a server to host a landing page. */
-		var win = window.open("https://www.facebook.com/v2.8/dialog/oauth?client_id=284227568629145&response_type=token&scope=email,public_profile,user_friends&redirect_uri=https://www.facebook.com/connect/login_success.html");
+		var win = window.open("https://www.facebook.com/v2.8/dialog/oauth?client_id=284227568629145&response_type=token&scope=email,public_profile,user_friends,user_birthday,user_education_history,user_friends,user_hometown,user_likes,user_work_history&redirect_uri=https://www.facebook.com/connect/login_success.html");
 	};
 	// After the login token is stored in storage via grabAccessToken.js, the GraphAPI can be queried.
 	document.getElementById("Get").onclick = function() {
 		// Retrieve the user access token from chrome storage
 		chrome.storage.sync.get("accessToken", function(item) {
 			// Make a GET request to the graph endpoint with user access token, once request completes call jsonParse to handle the response.
-			httpGetAsync("https://graph.facebook.com/me?fields=id,cover,first_name,last_name,age_range,gender,email&access_token="+item.accessToken, jsonParse);
+			httpGetAsync("https://graph.facebook.com/me?fields=id,cover,first_name,last_name,age_range,gender,email,work,education&access_token="+item.accessToken, jsonParse);
 		});
 		
 	};
