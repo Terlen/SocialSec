5b59120bbf0d82b589a1d1c637ec5043379129bc
Terlen <paidan@ymail.com>
2017-01-13 11:48:09 -0700

    Reverted

--- Basic Extension Template/wordcompare.js
+++ Basic Extension Template/wordcompare.js
@@ -4,19 +4,15 @@ chrome.runtime.onMessage.addListener(
 		console.log(sender.tab ?
 			"from a content script:" + sender.tab.url :
 			"from the extension");
-		if (request.greeting != null){
+		if (request.greeting == "alert"){
 			sendResponse({complete: "done"});
-		/*} else if (requeest.greeting = "secure"){
-			sendResponse({complete: "it's working"});
-			
-			chrome.browserAction.setIcon({path:"icon2.png"});
-			chrome.browserAction.setPopup({popup:"popup2.html"});
-	}*/}
+		}
 		wordMatch(request.greeting);
-	}
+
+		}
+
 )
-function wordMatch(text){
-	document.getElementById("submit").onclick = function() {
+/*	document.getElementById("submit").onclick = function() {
 		var submitstring = document.getElementById("text").value;
 		if (wordlist.indexOf(submitstring) > -1) {
 			alert("String matched");
@@ -24,4 +20,4 @@ function wordMatch(text){
 			alert("No exact match");
 		}
 	}
-}
+*/
