f5bd3408d67f5247707817f6bcb1595ed35da743
Terlen <paidan@ymail.com>
2017-01-13 12:12:43 -0700

    Implemeted changing icon and popup
    Added message sending between grabAccessToken.js and event page to
    trigger different popup when Facebook login is completed.

---
+++ Basic Extension Template/browserAction.js
@@ -0,0 +1,10 @@
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "secure")
+		sendResponse({farewell: "goodbye"});
+		chrome.browserAction.setIcon({path : "icon.png"});
+		chrome.browserAction.setPopup({popup : "popup2.html"});
+  });
