2c228820ca48c99cdf9f7bda627b1494ff2636d8
Terlen <paidan@ymail.com>
2017-02-01 12:54:25 -0700

    Comments
    Commented rounded profile picture code

--- Basic Extension Template/roundedimage.js
+++ Basic Extension Template/roundedimage.js
@@ -1,8 +1,10 @@
 window.onload = function() {
+	//  On popup load, fetch stored user data from Facebook. Grab user picture URL from data and pass to profile image injector.
 	chrome.storage.sync.get("userdata", function(item) {
 		profileImage(item.userdata.picture.data.url);
 	});
 }
 function profileImage(imgurl){
+	// Inject profile image URL into popup2 html
 	document.getElementById("profile").src = imgurl;
 }
