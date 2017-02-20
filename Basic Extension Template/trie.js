e02696d99ceb7f92585848ecc5968f308355acb1
Terlen <paidan@ymail.com>
2017-02-20 13:12:50 -0700

    Added comments to trie.js
    Added comments explaining functions and identifying debug code in
    trie.js.

--- Basic Extension Template/trie.js
+++ Basic Extension Template/trie.js
@@ -1,6 +1,7 @@
 // Trie data structure code is adapted from Ben Vallon's Computer Science in Javascript #data-structures series
 // His code can be found at https://github.com/benoitvallon/computer-science-in-javascript/blob/master/data-structures-in-javascript/trie.js
 
+// Node object creation. When called, creates Node object with given properties.
 function Node(data){
 	this.data = data;
 	this.isWord = false;
@@ -8,21 +9,26 @@ function Node(data){
 	this.children = {};
 }
 
+// Create a new trie structure with a root node containing and empty string.
 function Trie(){
 	this.root = new Node('');
 }
 
+// Defines trie method for adding a word to the structure. 
 Trie.prototype.add = function(node, word) {
   if(!node || !word) {
     return null;
   }
   node.prefixes++;
   var letter = word.charAt(0);
+// Check if the node already has a child with the given letter. If no child is found for that letter, create a new one and add it to the previous node's child property.
+// Important to note that although children was originally declared as an empty array, by using a named index and not a numerical, Javascript automatically converts to an object.
   var child = node.children[letter];
   if(!child) {
     child = new Node(letter);
     node.children[letter] = child;
   }
+// Determine how many characters are left in word to be added. If none, set isWord flag to true and complete. Else, recursively call add with new child node and rest of word.
   var remainder = word.substring(1);
   if(!remainder) {
     child.isWord = true;
@@ -30,6 +36,7 @@ Trie.prototype.add = function(node, word) {
   this.add(child, remainder);
 };
 
+// Debug function to list all words stored in the trie.
 Trie.prototype.getWords = function() {
   var words = [];
   var word = '';
@@ -49,16 +56,18 @@ Trie.prototype._getWords = function(node, words, word) {
   }
 };
 
-
+// Fetch stored data in Chrome storage. Once completed, call buildTrie and pass retrieved data.
 function trieData(){
 	chrome.storage.sync.get("userdata", buildTrie);
 }
 
+// Create trie structure with fetched data from Chrome storage.
 function buildTrie(data){
 	var trie = new Trie();
 	for (var x = 0; x < data.userdata.length; x++){
 			var value = (data.userdata[x]);
 			trie.add(trie.root,data.userdata[x].toString());
 		}
+// Debug code to list contents of trie.
 	alert(trie.getWords());
 }
