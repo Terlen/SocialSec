9645d73805a53ee9c59b7bd98d012d90467997de
Terlen <paidan@ymail.com>
2017-02-22 15:58:00 -0700

    Continued work on displaying word list
    Added functionality to list all stored words as a string on the HTML
    page.

--- Basic Extension Template/index.js
+++ Basic Extension Template/index.js
@@ -1,5 +1,22 @@
+var values;
 window.onload = function(){
+	
+	chrome.storage.sync.get("userdata",build);
+	
 	document.getElementById("xlist").onclick = function(){
-		document.getElementById("CusList").innerHTML = "TEXT";
+		var cusList;
+		for(var x =0; x < values.length; x++){
+			cusList += values;
+		}
+		document.getElementById("CusList").innerHTML = cusList;
+		
+
 }
+}
+
+
+function build(data){
+	alert(data.userdata[1]);
+	values = data.userdata;
+	alert("Values: " + values);
 }
