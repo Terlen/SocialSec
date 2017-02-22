d433fd195052dfd6a009d0c54f102d7262717c49
Terlen <paidan@ymail.com>
2017-02-22 15:40:54 -0700

    Work on populating HTML with storage data

--- Basic Extension Template/index.js
+++ Basic Extension Template/index.js
@@ -1,12 +1,5 @@
 window.onload = function(){
-	document.getElementById("xlist").addEventListener("onclick",inject);
+	document.getElementById("xlist").onclick = function(){
+		document.getElementById("CusList").innerHTML = "TEXT";
 }
-
-
-
-		
-function inject(){
-	chrome.storage.sync.get("userdata", function(item) {
-			document.getElementById("CusList").innerHTML = "text!";
-		});
 }
