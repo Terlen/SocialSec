7ae9e29bd5cf75d5486bebc7115a1cb52f9d1364
bhcunit6 <beaxelse@asu.edu>
2017-04-19 15:17:46 -0700

    Sync needed to work from desktop
    partial implementation of page WL

--- SocialSec/alertPhrase.js
+++ SocialSec/alertPhrase.js
@@ -2,4 +2,10 @@ window.onload = function(){
 	chrome.runtime.sendMessage({phrase: "Giff phrase"}, function(response){
 			document.getElementById("xphrase").innerHTML = response.phrase;
 		});
+		document.getElementById("WLpage").onclick = function(){
+		chrome.storage.local.get("currentURL",function(item){
+			var Surl = JSON.stringify(item.currentURL);
+			alert(Surl)
+		});
+}
 }
